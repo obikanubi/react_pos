f9c44d11892bcfc79338dd12d79d7c01a6bd62ad
Trev <trevor.c.cruz@gmail.com>
2017-09-13 11:59:51 +0800

    this.props being passed to componenes

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <div className="my_app">
-        <Items />
+        <Items items={this.state.items}/>
         <List />
         <Total />
       </div>
--- src/components/items.js
+++ src/components/items.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Items extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="app-items">
         <p> items on sale here </p>
