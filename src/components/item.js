9046971c848a8f35c72ab433413b2a0270f4977f
Trev <trevor.c.cruz@gmail.com>
2017-09-13 16:07:49 +0800

    some styling

--- src/components/item.js
+++ src/components/item.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import List from './list';
 
 class Item extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: []
+    }
+  }
 
   placeOrder() {
-    console.log("clicked");
+    return (
+      <List key={this.props.item.id} name={this.props.item.name} price={this.props.item.price}/>
+    )
   };
 
   render() {
     return (
-        <div className="col-4 box" onClick={this.placeOrder}>
+        <div className="col-4 box" onClick={this.placeOrder.bind(this)}>
           <strong>{this.props.item.name}</strong> <br />
           {this.props.item.price}
         </div>
--- src/components/list.js
+++ src/components/list.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 
 class List extends Component {
-  constructor() {
-    super();
-    this.state = {
-      list: []
-    }
-  }
-  
+
   render() {
     return (
-      <div className="app-list">
-        <div className="row">
+      <div className="row">
+        <div className="app-list">
+          {this.props.name}, {this.props.price}
         </div>
       </div>
     );
